8f6ba07c1f50d819f297a1c67378c9afaef52ba3
Jonathan D.A. Jewell <6759885+hyperpolymath@users.noreply.github.com>
2026-07-26 16:07:12 +0100

    chore: update guix.scm from squisher-corpus

--- guix.scm
+++ guix.scm
@@ -1,5 +1,5 @@
 ; SPDX-License-Identifier: MPL-2.0
-;; guix.scm — GNU Guix package definition for unified-dataset-vocab
+;; guix.scm — GNU Guix package definition for squisher-corpus
 ;; Usage: guix shell -f guix.scm
 
 (use-modules (guix packages)
@@ -7,12 +7,12 @@
              (guix licenses))
 
 (package
-  (name "unified-dataset-vocab")
+  (name "squisher-corpus")
   (version "0.1.0")
   (source #f)
   (build-system gnu-build-system)
-  (synopsis "unified-dataset-vocab")
-  (description "unified-dataset-vocab — part of the hyperpolymath ecosystem.")
-  (home-page "https://github.com/hyperpolymath/unified-dataset-vocab")
-  (license ((@@ (guix licenses) license) "MPL-2.0"
+  (synopsis "squisher-corpus")
+  (description "squisher-corpus — part of the hyperpolymath ecosystem.")
+  (home-page "https://github.com/hyperpolymath/squisher-corpus")
+  (license ((@@ (guix licenses) license) "PMPL-1.0-or-later"
              "https://github.com/hyperpolymath/palimpsest-license")))
